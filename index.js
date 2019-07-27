dfa20e55d714f34dfadfc219a3228177efc52570
Brandon <brandon.lau86@gmail.com>
2019-07-27 13:05:34 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = (arr) => 
+{
+  let answer = undefined;
+  arr.find(function(e)
+  {
+    if (e.result == "W")
+    {
+      console.log(e.year);
+      answer = e.year;
+    }
+  })
+
+  return answer;
+}
+
+superbowlWin();
